c580e769913786120f607a040fb4fc1ea4583676
Sanjay Kumar <72185623+sanjayhjp121@users.noreply.github.com>
2023-05-08 17:00:12 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 import './App.css';
+import Papa from 'papaparse';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState(null);
+
+  const fetchAndParseData = async () => {
+    const response = await axios.get('https://www.terriblytinytales.com/test.txt');
+    const words = response.data.split(/\s+/);
+    const frequencyMap = {};
+
+    words.forEach((word) => {
+      const lowerCaseWord = word.toLowerCase();
+      frequencyMap[lowerCaseWord] = (frequencyMap[lowerCaseWord] || 0) + 1;
+    });
+
+    const sortedWords = Object.entries(frequencyMap).sort((a, b) => b[1] - a[1]);
+    const topWords = sortedWords.slice(0, 20).map(([word, count]) => ({ word, count }));
+
+    setData(topWords);
+  };
+
+  const exportCSV = () => {
+    const csv = Papa.unparse(data);
+    const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+    const link = document.createElement('a');
+    const url = URL.createObjectURL(blob);
+    link.setAttribute('href', url);
+    link.setAttribute('download', 'word-frequency.csv');
+    link.style.visibility = 'hidden';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="buttons-container">
+        <button onClick={fetchAndParseData}>Submit</button>
+        {data && <button onClick={exportCSV}>Export</button>}
+      </div>
+      {data && (
+        <div className="chart-container">
+          <BarChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="word" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="count" fill="#8884d8" />
+          </BarChart>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
